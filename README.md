5c62ab17709a2e5881deff10a29fe66f23084f78
shuntaka <chang.shoon@gmail.com>
2017-01-24 23:50:25 +0900

    implement a simple photobooth

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# ReactWebcamhello  
+# ReactWebcam
+>this repository is for testing the idea from
+https://github.com/shuntaka/ReactWebcam and
+https://www.youtube.com/watch?v=gA_HJMd7uvQ
--- js/photo.js
+++ js/photo.js
@@ -1,5 +1,8 @@
 (function(){
   var video = document.getElementById('video'),
+      canvas = document.getElementById('canvas'),
+      context = canvas.getContext('2d'),
+      photo = document.getElementById('photo'),
       vendorUrl = window.URL || window.webkitURL;
   navigator.getMedia = navigator.getUserMedia ||
     navigator.webkitGetUserMedia ||
@@ -15,5 +18,10 @@
   }, function(error) {
     // An error occured
     // error.code
+  });
+
+  document.getElementById('capture').addEventListener('click', function() {
+    context.drawImage(video, 0, 0, 400, 300);
+    photo.setAttribute('src', canvas.toDataURL('img/png'));
   });
 })();
