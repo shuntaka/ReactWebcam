3eaa3748d0ffd972a71ef493134b3946c1804bc9
takamiya shunsuke <shunsuke.takamiya@ntt.com>
2017-01-27 17:55:59 +0900

    webpack config

---
+++ app/components/Gallery.js
@@ -0,0 +1,10 @@
+import React, { Component, PropTypes } from 'react';
+
+import { render } from 'react-dom';
+import Gallery from './components/Gallery';
+
+
+render(
+    <Gallery />
+    document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+
+/*
+ * Default webpack configuration for development
+ */
+var config = {
+  devtool: 'eval-source-map',
+  entry:  __dirname + "/app/App.js",
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /masonry|imagesloaded|fizzy\-ui\-utils|desandro\-|outlayer|get\-size|doc\-ready|eventie|eventemitter/,
+        loader: 'imports?define=>false&this=>window'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: "./public",
+    colors: true,
+    historyApiFallback: true,
+    inline: true
+  },
+}
